9969427c348daea346ac0a3f38c6206c6766e5a7
Xenoas1er <brunoethan6@gmail.com>
2024-10-22 10:52:36 +0200

    Buyoyancy works

--- An Era of the Seas/Assets/Scripts/Floater.cs
+++ An Era of the Seas/Assets/Scripts/Floater.cs
@@ -8,12 +8,21 @@ public class Floater : MonoBehaviour
     public Rigidbody rigidbody;
     public float depthbeforesubmerge = 1f;
     public float displacememtAmount = 3f;
+    public float floatercount = 1;
+
+    public float waterDrag = 0.99f;
+    public float waterAngularDrag = 0.5f;
     private void FixedUpdate(){
 
+
+        rigidbody.AddForceAtPosition(Physics.gravity / floatercount, transform.position, ForceMode.Acceleration);
         float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x);
         if(transform.position.y < waveHeight){
             float displacememtMultiplier = Mathf.Clamp01((waveHeight-transform.position.y)/ depthbeforesubmerge) * displacememtAmount;
-            rigidbody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacememtAmount, 0f), transform.position,ForceMode.Acceleration);
+            rigidbody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacememtMultiplier, 0f), transform.position,ForceMode.Acceleration);
+            rigidbody.AddForce(displacememtMultiplier * -rigidbody.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rigidbody.AddTorque(displacememtMultiplier * -rigidbody.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            
         }
 
         
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # An-Era-of-the-Seas
 
-This GitHub repo is storing our Project for S1S2.
+This GitHub repo is storing our Project for S2.
 More about it later...
