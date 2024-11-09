dbaddc03f0def5bc7a525d137994e5097d485199
Starlight <linjerome95@gmail.com>
2024-11-09 18:31:21 +0100

    Stats update

---
+++ Assets/Scripts/Items/Crew.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Unity.Plastic.Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace Items
+{
+    public class Crew
+    {
+        public string Name;
+        public string Rarity;
+        public int MaxLevel;
+        public int Price;
+        public List<string> Stats;
+    }
+
+    public class CrewList
+    {
+        public List<Crew> Crews;
+    }
+
+
+
+
+
+
+
+
+    
+    
+    public class CrewLoader
+    {
+        public List<Crew> LoadCrew()
+        {
+            string path = Resources.Load<TextAsset>("Stats/items/crew.json").text;
+            JObject crewData = JObject.Parse(File.ReadAllText(path));
+            CrewList crewList = new CrewList();
+            foreach (var crew in (JObject)crewData["Crew"])
+            {
+                foreach (var rarity in (JObject)crewData["Rarity"])
+                {
+                    Crew newCrew = new Crew();
+                    newCrew.Name = crew.Key;
+                    newCrew.Rarity = rarity.Key;
+                    newCrew.MaxLevel = (int)rarity.Value["Max Level"];
+                    newCrew.Price = (int)rarity.Value["Price"];
+                    newCrew.Stats = crew.Value["Stats"].ToObject<List<string>>();
+                    
+                    crewList.Crews.Add(newCrew);
+                }
+            }
+            return crewList.Crews;
+        }
+    }
+}
---
+++ Assets/Scripts/Items/Weapon.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Unity.Plastic.Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace Items
+{
+    public class Weapon
+    {
+        public string Name;
+        public string Rarity;
+        public int MaxLevel;
+        public int Price;
+        public List<string> Stats;
+    }
+    
+    public class WeaponList
+    {
+        public List<Weapon> Swords;
+        public List<Weapon> Guns;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    public class SwordLoader
+    {
+        public List<Weapon> LoadSwords()
+        {
+            string path = Resources.Load<TextAsset>("Stats/items/weapon.json").text;
+            JObject weaponData = JObject.Parse(File.ReadAllText(path));
+            WeaponList swordList = new WeaponList();
+            foreach (var sword in (JObject)weaponData["Swords"])
+            {
+                Weapon newSword = new Weapon();
+                newSword.Name = sword.Key;
+                newSword.Rarity = (string)sword.Value["Rarity"];
+                newSword.MaxLevel = (int)weaponData["Rarity"][newSword.Rarity]["Max Level"];
+                newSword.Price = (int)weaponData["Rarity"][newSword.Rarity]["Price"];
+                newSword.Stats = sword.Value["Stats"].ToObject<List<string>>();
+                
+                swordList.Swords.Add(newSword);
+            }
+            return swordList.Swords;
+        } 
+    }
+
+
+
+
+
+
+
+
+    
+    public class GunLoader
+    {
+        public List<Weapon> LoadGuns()
+        {
+            string path = Resources.Load<TextAsset>("Stats/items/weapon.json").text;
+            JObject weaponData = JObject.Parse(File.ReadAllText(path));
+            WeaponList gunList = new WeaponList();
+            foreach (var gun in (JObject)weaponData["Guns"])
+            {
+                Weapon newGun = new Weapon();
+                newGun.Name = gun.Key;
+                newGun.Rarity = (string)gun.Value["Rarity"];
+                newGun.MaxLevel = (int)weaponData["Rarity"][newGun.Rarity]["Max Level"];
+                newGun.Price = (int)weaponData["Rarity"][newGun.Rarity]["Price"];
+                newGun.Stats = gun.Value["Stats"].ToObject<List<string>>();
+                
+                gunList.Guns.Add(newGun);
+            }
+            return gunList.Guns;
+        }
+    }
+}
