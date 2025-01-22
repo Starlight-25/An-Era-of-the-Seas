ae203ead10455b04fb4ed68bd0c4bcad7153a990
Starlight <linjerome95@gmail.com>
2025-01-22 19:53:08 +0100

    Cursor mode added

--- Assets/Scripts/Network/Movements/MouseLook.cs
+++ Assets/Scripts/Network/Movements/MouseLook.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    
     public float mouseSensitivity = 100f;    
-    
     public Transform playerBody;
-    
     float xRotation = 0f;
+    
+    
+    
+    
     
     void Start()
     {
@@ -18,13 +19,34 @@ public class MouseLook : MonoBehaviour
     
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        MouseAltMode();
         
-        xRotation -= mouseY; // Rotate the CAMERA on the Y axis
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // So that the player doesn't become an owl (can't move the camera more than 90°)
-        
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX); // Rotate the CHARACTER on the X axis
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY; // Rotate the CAMERA on the Y axis
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f); // So that the player doesn't become an owl (can't move the camera more than 90°)
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX); // Rotate the CHARACTER on the X axis
+        }    
+    }
+
+    
+    
+    
+    
+    private void MouseAltMode()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 }
