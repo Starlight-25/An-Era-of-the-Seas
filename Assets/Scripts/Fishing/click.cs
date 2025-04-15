b9287fdfb186767d13f987b1fa45d5229e7da729
Akma333 <168654719+Akma333@users.noreply.github.com>
2025-04-15 20:37:52 +0200

    new version

--- Assets/Scripts/Fishing/click.cs
+++ Assets/Scripts/Fishing/click.cs
@@ -1,30 +1,43 @@
 using UnityEngine;
-
 public class click : MonoBehaviour
 {
     private Renderer _rend;
     private Transform transform;
     int cpt;
-    float i ;
+    float x ;
+    float y ;
+    int shitty;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rend = GetComponent<Renderer>();
         transform = GetComponent<Transform>();
-        i=transform.position[0];
+        x=transform.position[0];
+        y = transform.position[1];
         cpt=0;
-    }
+
+    }//
 
     // Update is called once per frame
     void Update()
     {
         
-        transform.position=new Vector3((float)i,transform.position[1],transform.position[2]);
-        i=i + (float)0.005;
-        if(i>9){
-            i=-9;
+        transform.position=new Vector3((float)x,y,transform.position[2]);
+        
+        x+= (float)0.05;
+        
+        
+        y+=(Mathf.Sin(x)/20) ;
+        
+        
+        if(x>9){
+            x=-9;
+            y = Random.Range(-3.0f, 5.0f);
         }
-    
+
+        shitty += 1;
+
     }
     void OnMouseDown(){
         Debug.Log("Click!!!");
