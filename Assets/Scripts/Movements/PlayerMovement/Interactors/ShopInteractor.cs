8d5c5b732f632354a14da43847c84d82ebfa143b
Starlight <linjerome95@gmail.com>
2025-05-01 12:20:23 +0200

    Shop interactor fixed

--- Assets/Scripts/Movements/PlayerMovement/Interactors/ShopInteractor.cs
+++ Assets/Scripts/Movements/PlayerMovement/Interactors/ShopInteractor.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public static class ShopInteractor
 {
-    private static PlayerUIManager playerUIManager = GameObject.Find("UI").transform.Find("Player UI (Canvas)").GetComponent<PlayerUIManager>();
-    private static GameObject ShopCanvas = GameObject.Find("UI").transform.Find("Shop (Canvas)").gameObject;
+    private static PlayerUIManager playerUIManager = GameObject.Find("UI").transform.Find("Player UI Menu").GetComponent<PlayerUIManager>();
+    private static GameObject ShopCanvas = GameObject.Find("UI").transform.Find("Shop Menu").gameObject;
 
     public static void ShowShop(string type)
     {
