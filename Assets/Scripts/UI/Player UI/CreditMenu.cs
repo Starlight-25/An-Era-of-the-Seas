b0f4fa0c9c025a52a412ed79d382f45871d308cc
Starlight <linjerome95@gmail.com>
2025-05-17 14:14:05 +0200

    Credits text added to the Credits menu

--- Assets/Scripts/UI/Player UI/CreditMenu.cs
+++ Assets/Scripts/UI/Player UI/CreditMenu.cs
@@ -1,12 +1,15 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
 
 
-public class CreditMenu : MonoBehaviour
+public class CreditMenu : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private GameObject CreditCanvas;
     [SerializeField] private GameObject SettingsCanvas;
+    [SerializeField] private TextMeshProUGUI CreditsText;
 
     public void ReturnButtonClicked()
     {
@@ -19,6 +22,17 @@ public class CreditMenu : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             ReturnButtonClicked();
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(CreditsText, eventData.position, null);
+        if (linkIndex != -1)
+        {
+            TMP_LinkInfo linkInfo = CreditsText.textInfo.linkInfo[linkIndex];
+            string linkId = linkInfo.GetLinkID();
+            Application.OpenURL(linkId);
         }
     }
 }
